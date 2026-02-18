3b6dac92f41122aeb3905c986e077db4b1963350
Sravanthi-Elakoti <sravanthielakoti@gmail.com>
2026-02-18 15:10:11 +0530

    Initial commit

---
+++ src/main/java/Appspring/AppSpring.java
@@ -0,0 +1,70 @@
+package Appspring;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.*;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@SpringBootApplication
+public class AppSpring implements CommandLineRunner {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppSpring.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        RestTemplate restTemplate = new RestTemplate();
+
+        String url = "https://bfhldevapigw.healthrx.co.in/hiring/generateWebhook/JAVA";
+
+        Map<String, String> requestBody = new HashMap<>();
+        requestBody.put("name", "Your Name");
+        requestBody.put("regNo", "REG12347");
+        requestBody.put("email", "your@email.com");
+
+        ResponseEntity<Map> response =
+                restTemplate.postForEntity(url, requestBody, Map.class);
+
+        Map responseBody = response.getBody();
+        String accessToken = (String) responseBody.get("accessToken");
+
+        String finalQuery ="SELECT d.DEPARTMENT_NAME, "
+                + "AVG(TIMESTAMPDIFF(YEAR, e.DOB, CURDATE())) AS AVERAGE_AGE, "
+                + "SUBSTRING_INDEX(GROUP_CONCAT(CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME) "
+                + "ORDER BY e.EMP_ID SEPARATOR ', '), ', ', 10) AS EMPLOYEE_LIST "
+                + "FROM DEPARTMENT d "
+                + "JOIN EMPLOYEE e ON d.DEPARTMENT_ID = e.DEPARTMENT "
+                + "JOIN PAYMENTS p ON e.EMP_ID = p.EMP_ID "
+                + "WHERE p.AMOUNT > 70000 "
+                + "GROUP BY d.DEPARTMENT_ID, d.DEPARTMENT_NAME "
+                + "ORDER BY d.DEPARTMENT_ID DESC";
+
+
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Authorization", accessToken);
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        Map<String, String> answerBody = new HashMap<>();
+        answerBody.put("finalQuery", finalQuery);
+
+        HttpEntity<Map<String, String>> entity =
+                new HttpEntity<>(answerBody, headers);
+
+        ResponseEntity<String> finalResponse =
+                restTemplate.postForEntity(
+                        "https://bfhldevapigw.healthrx.co.in/hiring/testWebhook/JAVA",
+                        entity,
+                        String.class
+                );
+
+        System.out.println(finalResponse.getBody());
+    }
+}
+
